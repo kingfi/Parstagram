59fb4c53dd8d24af3e932cad85630d57f0ed5f45
Fianko Buckle <fiankobuckle@gmail.com>
2019-11-15 20:30:00 -0500

    more

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import AlamofireImage
+import Parse
 
-class CameraViewController: UIViewController, UIImagePickerControllerDelegate {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var commentField: UITextField!
@@ -22,11 +23,31 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate {
     }
     
     @IBAction func onSubmit(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = commentField.text!
+        post["author"] = PFUser.current()
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("saved")
+            } else {
+                print("error!")
+            }
+        }
+        
+        
     }
     
     @IBAction func onCameraButton(_ sender: Any) {
         let picker = UIImagePickerController()
-        picker.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        picker.delegate = self
         picker.allowsEditing = true
         
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
